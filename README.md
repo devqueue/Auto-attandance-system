896ec2066e3d13f71115a2520c01058de69d2d26
smokedpirate <smokedpirate@protonmail.com>
2020-10-04 13:38:58 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Face Recognition using open-cv
+
+This is a project that recognizes your face using webcam and adds your attandance to a csv file
+I used open-cv and face-recognition libraries. The faces must be saved in individual directories
+in `src/images`. There must be atleast 8-10 images for each face with clear features.
+Follow the steps below to get started.
+
+**Note:** Before following along you must have a c/c++ compiler on your system if you use windows use visual studio to install C++ compiler
+
+## How to Install dependencies?
+
+#### Using anaconda environment.yml (linux only)
+
+The conda_environment.yml was created on a linux machine so I would recommend you not to use this unless you are on linux or if you can manually delete the linux dependencies that'll work fine as well
+
+1. Open conda_environment.yml
+2. Replace the first line `name: FaceRec` to your prefered name
+3. Replace 'user' with your username in `prefix: /home/user/.conda/envs/FaceRec`
+4. cd into the folder
+5. Now open terminal and run
+`conda env create -f conda_environment.yml`
+
+#### Using pip (For any OS)
+
+1. Create a virtual environment
+2. Run `pip install -r requirements.txt`
+
+## Setup for a new face
+
+1. Go to src/images directory
+2. Create a directory with your name
+3. Add 8-10 images of your face in different lighiting conditions
+4. Now run `face-trainer.py`
+Note: Everytime you add a new face you'll have to run this file again
+
+## Final Recognition
+
+Now you can run the `face-detect.py`. The script will recognize your face and add a time stamp
+in `Attendance.csv`
+
+## Run in Gitpod
+
+You can also run Face-detector in Gitpod, a free online dev environment for GitHub:
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/smokedpirate/Face-recognition/blob/master/src/face-detect.py)
---
+++ src/neuralfacedetect.py
@@ -0,0 +1,126 @@
+#import neuralnetwork
+import keras
+import utils
+import glob
+import os
+import cv2
+import numpy as np
+# Load weights from csv files (which was exported from Openface torch model)
+weights = utils.weights
+weights_dict = utils.load_weights()
+
+# load the model
+model = keras.models.load_model("neural-network.h5")
+# Set layer weights of the model
+for name in weights:
+  if model.get_layer(name) != None:
+    model.get_layer(name).set_weights(weights_dict[name])
+  elif model.get_layer(name) != None:
+    model.get_layer(name).set_weights(weights_dict[name])
+
+
+def image_to_embedding(image, model):
+    #image = cv2.resize(image, (96, 96), interpolation=cv2.INTER_AREA)
+    image = cv2.resize(image, (96, 96))
+    img = image[..., ::-1]
+    img = np.around(np.transpose(img, (0, 1, 2))/255.0, decimals=12)
+    x_train = np.array([img])
+    embedding = model.predict_on_batch(x_train)
+    return embedding
+
+
+def recognize_face(face_image, input_embeddings, model):
+
+    embedding = image_to_embedding(face_image, model)
+
+    minimum_distance = 200
+    name = None
+
+    # Loop over  names and encodings.
+    for (input_name, input_embedding) in input_embeddings.items():
+
+        euclidean_distance = np.linalg.norm(embedding-input_embedding)
+
+        #print('Euclidean distance from %s is %s' % (input_name, euclidean_distance))
+
+        if euclidean_distance < minimum_distance:
+            minimum_distance = euclidean_distance
+            name = input_name
+
+    if minimum_distance < 0.68:
+        return str(name)
+    else:
+        return None
+
+
+def create_input_image_embeddings():
+    input_embeddings = {}
+
+    for file in glob.glob("images/*"):
+        person_name = os.path.splitext(os.path.basename(file))[0]
+        image_file = cv2.imread(file, 1)
+        input_embeddings[person_name] = image_to_embedding(image_file, model)
+
+    return input_embeddings
+
+
+def markattendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        dataList = f.readlines()
+        nameList = []
+        for line in dataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            date = now.strftime('%b %d %Y')
+            day = now.strftime('%a')
+            time = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name}, {date}, {time}, {day}')
+        
+input_embeddings = create_input_image_embeddings()
+# def recognize_faces_in_cam(input_embeddings):
+
+cv2.namedWindow("Face Recognizer")
+cap = cv2.VideoCapture(0)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+face_cascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+
+while cap.isOpened():
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]  # (ycord_start, ycord_end)
+        roi_color = frame[y:y+h, x:x+w]
+        #print(w, h)
+
+    	# recognize
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 4 and conf <= 85:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            colortext = (255, 255, 255)
+            colorframe = (255, 0, 0)
+            stroke = 2
+            #bestscale = font_scale(name, w)
+            #print(bestscale)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), colorframe, stroke)
+            cv2.rectangle(frame, (x, y+h), (x+w, y+h+20), colorframe, stroke)
+            cv2.rectangle(frame, (x, y+h), (x+w, y+h+20),
+                          colorframe, cv2.FILLED)
+            cv2.putText(frame, name, (x, y+h+20),
+                        font, 0.70, colortext, stroke)
+            markattendance(name)
+
+    # Display the resulting frame
+    cv2.imshow('Face-detector', frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
